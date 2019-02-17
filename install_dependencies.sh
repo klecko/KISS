3b7cad40ab75520d80fd104d2856b3a479708c4e
David Mateos <davidete345@gmail.com>
2019-02-17 20:28:58 +0100

    Improved install dependencies script

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -6,13 +6,21 @@ ENDC="\033[0m"
 
 echo -e "$CIAN[KISS]$ENDC Running apt update..."
 apt update
-echo -e "$CIAN[KISS]$ENDC Installing python3..."
+echo -e "\n$CIAN[KISS]$ENDC Installing python3..."
 apt install python3
-echo -e "$CIAN[KISS]$ENDC Installing pip..."
-apt install python3-pip
-echo -e "$CIAN[KISS]$ENDC Installing scapy..."
-python3 -m pip install --no-cache-dir scapy
-echo -e "$CIAN[KISS]$ENDC Copying custom scapy files to scapy directory..."
+echo -e "\n$CIAN[KISS]$ENDC Installing python3-distutils..."
+apt install python3-distutils
+
+echo -e "\n$CIAN[KISS]$ENDC Installing scapy..."
+mkdir tmp
+wget -O tmp/master.zip --trust-server-names https://github.com/secdev/scapy/archive/master.zip 
+unzip tmp/master.zip -d tmp/
+cd tmp/scapy-master/
+python3 setup.py install
+cd ../..
+rm -r tmp
+
+echo -e "\n$CIAN[KISS]$ENDC Copying custom scapy files to scapy directory..."
 scapy_path=$(python3 -c "import scapy, os; print(os.path.dirname(os.path.realpath(scapy.__file__)))")
 cp -r scapy_files/* $scapy_path
-echo -e "$CIAN[KISS]$ENDC DONE."
+echo -e "\n$CIAN[KISS]$ENDC DONE."
